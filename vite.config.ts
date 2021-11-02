11091e65ea5cb80a8605d81cc01f0d823391272c
itchymax <itchymax@itchysats.network>
2021-11-02 12:17:23 +1100

    Specify target dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, `index.html`),
+        },
+        outDir: `dist/taker`,
+    },
     server: {
         proxy: {
             "/api": "http://localhost:8000",
